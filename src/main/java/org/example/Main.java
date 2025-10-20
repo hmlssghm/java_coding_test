131777b2c5acd3cece5ada7a78775f3ac8db29c3
hmlssghm <kwonse326@gmail.com>
2025-10-21 00:40:42 +0900

    feat: 요구 사항 구현 클래스 작성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,38 +1,66 @@
 package org.example;
 
-import javax.swing.plaf.synth.SynthEditorPaneUI;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     private void solution() throws IOException {
-        List<int[]> input = Main.InputView.makeList();
-        System.out.println(input);
+        List<List<Integer>> input = Main.InputView.makeList();
 
-//        List<Integer> numbers = Arrays.asList(5, 2, 8, 1, 3);
-//        int max = Collections.max(numbers);
-//        System.out.println(max);
+        Comparison.compare(input);
+    }
+
+    private static class Comparison {
+        private static void compare(List<List<Integer>> input) {
+            System.out.println(input.size());
+            // TODO: 출력 기능 분리
+            for(int i = 0; i < input.size(); i++) {
+                List<Integer> numbers = input.get(i);
+
+                Set<Integer> setNumbers = new HashSet<>(numbers);
+                int setSize = setNumbers.size();
+
+                int max = Collections.max(numbers);
+                numbers.remove(Integer.valueOf(max));
+                int left = numbers.stream().mapToInt(Integer::intValue).sum();
+
+                if (max >= left) {
+                    System.out.println("Invalid");
+                    continue;
+                }
+                if (setSize == 1) {
+                    System.out.println("Equilateral");
+                    continue;
+                }
+                if (setSize == 2) {
+                    System.out.println("Isosceles");
+                    continue;
+                }
+                if (setSize == 3) {
+                    System.out.println("Scalene");
+                }
+            }
+        }
     }
 
     private static class InputView {
         private static final String SEPARATOR = " ";
-        private static final int[] END_CONDITION = {0, 0, 0};
+        private static final List<Integer> END_CONDITION = Arrays.asList(0, 0, 0);
 
-
-        private static int[] read() throws IOException {
+        private static List<Integer> read() throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            return Arrays.stream(br.readLine().split(SEPARATOR)).mapToInt(Integer::parseInt).toArray();
+            return Arrays.stream(br.readLine().split(SEPARATOR)).map(Integer::parseInt).collect(Collectors.toList());
         }
 
-        private static List<int[]> makeList() throws IOException {
-            List<int[]> input = new ArrayList<>();
+        private static List<List<Integer>> makeList() throws IOException {
+            List<List<Integer>> input = new ArrayList<>();
 
             while (true) {
-                int[] lines = Main.InputView.read();
-                if (Arrays.equals(END_CONDITION, lines)) break;
+                List<Integer> lines = Main.InputView.read();
+                if (END_CONDITION.equals(lines)) break;
                 input.add(lines);
             }
 
@@ -40,14 +68,14 @@ public class Main {
         }
     }
 
-    private static class OutputView {
-        private static void printAnswer() {
+//    private static class OutputView {
+//        private static void printAnswer() {
 //            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 //            bw.write();
 //            bw.newline();
 //            bw.close();
-        }
-    }
+//        }
+//    }
 
     public static void main(String[] args) throws Exception {
         new Main().solution();
