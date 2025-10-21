4ad282e836ca6c35b6f026031caabc87bc71650a
hmlssghm <kwonse326@gmail.com>
2025-10-21 22:55:34 +0900

    fix: bufferedReader NullPointer Exception 안뜨게 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,29 +8,33 @@ import java.util.stream.Collectors;
 
 public class Main {
     private void solution() throws IOException {
-        List<List<Integer>> input = Main.InputView.makeList();
+        List<List<Integer>> input = Main.InputView.read();
+        for (int i = 0; i < input.size(); i++) {
+            System.out.println(input.get(i));
+        }
 
         Comparison.compare(input);
     }
 
     private static class Comparison {
         private static void compare(List<List<Integer>> input) {
-            System.out.println(input.size());
             // TODO: 출력 기능 분리
             for(int i = 0; i < input.size(); i++) {
                 List<Integer> numbers = input.get(i);
 
-                Set<Integer> setNumbers = new HashSet<>(numbers);
-                int setSize = setNumbers.size();
-
                 int max = Collections.max(numbers);
                 numbers.remove(Integer.valueOf(max));
                 int left = numbers.stream().mapToInt(Integer::intValue).sum();
+                System.out.println(left);
 
                 if (max >= left) {
                     System.out.println("Invalid");
                     continue;
                 }
+
+                Set<Integer> setNumbers = new HashSet<>(numbers);
+                int setSize = setNumbers.size();
+
                 if (setSize == 1) {
                     System.out.println("Equilateral");
                     continue;
@@ -47,24 +51,27 @@ public class Main {
     }
 
     private static class InputView {
-        private static final String SEPARATOR = " ";
         private static final List<Integer> END_CONDITION = Arrays.asList(0, 0, 0);
-
-        private static List<Integer> read() throws IOException {
+료
+        private static List<List<Integer>> read() throws IOException {
+            // TODO: eof 읽고 nullPointer EXception 안나게 처리
+            // TODO: 0 0 0 입력되면 입력 종
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            return Arrays.stream(br.readLine().split(SEPARATOR)).map(Integer::parseInt).collect(Collectors.toList());
-        }
 
-        private static List<List<Integer>> makeList() throws IOException {
-            List<List<Integer>> input = new ArrayList<>();
+            List<List<Integer>> numbers = new ArrayList<>();
+            String line;
 
-            while (true) {
-                List<Integer> lines = Main.InputView.read();
-                if (END_CONDITION.equals(lines)) break;
-                input.add(lines);
+            while ((line = br.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(line, " ");
+
+                List<Integer> separatedLine = new ArrayList<>();
+                while (st.hasMoreTokens()) {
+                    separatedLine.add(Integer.parseInt(st.nextToken()));
+                }
+
+                numbers.add(separatedLine);
             }
-
-            return input;
+            return numbers;
         }
     }
 
